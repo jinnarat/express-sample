7d1135d9b75574295842eadb43ea6660546b23c2
Jinnarat <jinnarat_yo@hotmail.com>
2019-03-31 10:40:46 +0700

    update

--- controller/testController.js
+++ controller/testController.js
@@ -8,20 +8,40 @@ let responseObject = {
     result: ""
 }
 
-router.get("/getData",  (req, res) => {
-    let result = req.query.key1 * req.query.key2;
-    res.send({ result: result })
-});
+// router.get("/getData", (req, res) => {
+//     let result = req.query.key1 * req.query.key2;
+//     res.send({ result: result })
+// });
+
+
+router.get("/getBMI", (req, res) => {
+    let height = req.query.height / 100
+    let weight = req.query.weight
+    let result = weight / (height * height);
+    let msg = "";
+    if (result > 25) {
+        msg = "Fat"
+    } else if (result == 25) {
+        msg = "Normal"
+    } else if (result < 25) {
+        msg = "Thin"
+    }
+
+    result = Namber(result.toFixed(2));
+    res.send({ bmi: result, msg: msg })
+
+})
 /*
 Request
 * */
-router.post("/postData",async  (req, res) => {
-    if(!req.body.key5){
+router.post("/postData", async (req, res) => {
+    if (!req.body.key5) {
         responseObject.msg = "Bad request";
-    }else{
+    } else {
         responseObject.msg = "success";
-        responseObject.result = req.body.key * 5;
+        responseObject.result = req.body.key5 * 5;
     }
+
     res.send(responseObject);
 });
 
